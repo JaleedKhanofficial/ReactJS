33b19385f259d42976ba84f540df26c4695d683c
Jaleed Khan <jaleedkhan53@gmail.com>
2025-11-24 11:03:32 +0500

    Created API for fetched data from backend and show in table form.

--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -1,5 +1,45 @@
+import { useEffect, useState } from "react"
+import http from "../../../http";
+
 export default function Home(){
+    const [users, setUsers] = useState([]);
+
+    useEffect(()=>{
+        fetchAllUsers();
+    },[]);
+
+    const fetchAllUsers = () =>{
+        http.get('./users').then(res=>{
+            setUsers(res.data);
+        })
+    }
+
+
     return(
-        <div>This is Home PAge</div>
+        <div>
+            <h2>User Listing.</h2>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>S.NO</th>
+                        <th>Name</th>
+                        <th>Created At</th>
+                        <th>Email</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {users.map((user,index) =>(
+                    <tr key={user.id}>
+                        <td>{++index}</td>
+                        <td>{user.name}</td>
+                        <td>{user.created_at}</td>
+                        <td>{user.email}</td>
+                        <td>EDIT</td>
+                    </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
     )
 }
