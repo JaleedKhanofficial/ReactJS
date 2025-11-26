dbbd4a372480a71e9a04ebdcba86e5b958229c4e
Jaleed Khan <jaleedkhan53@gmail.com>
2025-11-26 11:22:31 +0500

    fixing

--- http.jsx
+++ http.jsx
@@ -1,7 +1,9 @@
 import axios from "axios";
+
 export default axios.create({
-    baseURL:"http://127.0.0.1:8000/api",
-    headers:{
-        "content-type":"appliction/json"
+    baseURL: "http://127.0.0.1:8000/api",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
     }
-})
+});
